366fe229e87c7f1f4f8485631187d981fa235bed
JohnAssebe <johnassebe@gmail.com>
2025-02-18 12:41:06 -0900

    chore: comment ReactQueryDev tools for proxy setup :sparkles:

--- src/main.tsx
+++ src/main.tsx
@@ -7,7 +7,7 @@ import { Provider } from "react-redux";
 import { store } from "./store/store";
 import "react-toastify/dist/ReactToastify.css";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+// import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { ToastContainer } from "react-toastify";
 
 const queryClient = new QueryClient({
@@ -24,10 +24,10 @@ createRoot(document.getElementById("root")!).render(
         <Provider store={store}>
             <QueryClientProvider client={queryClient}>
                 <App />
-                <ReactQueryDevtools
-                    initialIsOpen={false}
-                    buttonPosition="bottom-left"
-                />
+                {/*<ReactQueryDevtools*/}
+                {/*    initialIsOpen={false}*/}
+                {/*    buttonPosition="bottom-left"*/}
+                {/*/>*/}
                 <ToastContainer position="top-right" autoClose={4000} />
             </QueryClientProvider>
         </Provider>
